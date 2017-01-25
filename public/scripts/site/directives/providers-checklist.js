067c34c1a918ca671d565b39155d77d76d33ee97
Cristian Cortez <cortez.cristian@gmail.com>
2017-01-25 19:57:31 -0300

    Providers checklist on its way

--- public/scripts/site/directives/providers-checklist.js
+++ public/scripts/site/directives/providers-checklist.js
@@ -10,6 +10,7 @@ angular.module('anyandgoApp')
   .directive('providersChecklist', function () {
     return {
       restrict: 'AC',
+      templateUrl: '/scripts/site/views/providers-checklist.html',
       link: function postLink($scope, element, attrs) {
       }
     };
