7ce06b29b047faee500860d946d4a75f69d480e3
Cristian Cortez <cortez.cristian@gmail.com>
2017-01-25 21:18:46 -0300

    Creating and listing providers

--- public/scripts/site/directives/providers-checklist.js
+++ public/scripts/site/directives/providers-checklist.js
@@ -11,6 +11,27 @@ angular.module('anyandgoApp')
     return {
       restrict: 'AC',
       templateUrl: '/scripts/site/views/providers-checklist.html',
+      controller: function($scope, $log, Restangular, toastr){
+        $log.log('Started!');
+        $scope.newprovider = "";
+        $scope.list = [];
+
+        $scope.createProvider = function(){
+          if($scope.newprovider !== ""){
+            Restangular.all('providers').post({name: $scope.newprovider}).then(function(providers) {
+              toastr.info('New provider was created', 'Operation Success');
+              $scope.refreshProvidersList();
+            });
+          }
+        }
+
+        $scope.refreshProvidersList = function() {
+          $scope.list = Restangular.all("providers").getList().$object;
+        }
+
+        // Fetch List
+        $scope.refreshProvidersList();
+      },
       link: function postLink($scope, element, attrs) {
       }
     };
